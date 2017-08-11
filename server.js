aad236d4aa20d9eed347f13572b10c056e646a88
Ujwala Sawant <ujwala1512@gmail.com>
2017-08-11 15:57:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("Article two will be requestsed and will be responed here");
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send("Article three will be requestsed and will be responed here");
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
