18b344676652d15d1df6c0b23e8c26edb7bdcb54
Ujwala Sawant <ujwala1512@gmail.com>
2017-08-13 18:53:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,11 +88,17 @@ var htmlTemplate =
 }
 
 
-
+var counter=0;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  res.send(counter.toString());
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
