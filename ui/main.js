fae8196c25d1c37a3e20ff724951bb3d18e2c1cc
Ujwala Sawant <ujwala1512@gmail.com>
2017-08-13 19:06:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var counter=0;
 
 var button=document.getElementById("counter");
 button.onclick=function(){
-   
+   alert("Hello");
     counter=counter+1;
     var count=document.getElementById("count");
     
